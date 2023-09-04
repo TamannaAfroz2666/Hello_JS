f1a03d57df91b823237fd025ee23d2bda37f7ee6
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-05 00:53:02 +0600

    hello js

---
+++ base.js
@@ -0,0 +1,11 @@
+console.log('hello world');
+function Multiply() {
+    let num1 =document.getElementById('first_value').value;
+    console.log('fiesr number is:', num1);
+    let num2 = document.getElementById('second_value').value;
+    console.log('fiesr number is:', num2);
+     let results = document.getElementById('result').innerHTML= num1 * num2;
+     console.log('multiply is:', results);
+    
+    
+}

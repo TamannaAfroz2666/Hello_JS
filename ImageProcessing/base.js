76d41ed68ca993edfe4f919dcdf935569aa15519
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-07 10:51:55 +0600

    add access key and api

--- ImageProcessing/base.js
+++ ImageProcessing/base.js
@@ -1,4 +1,15 @@
+const accesKey = "weUuR1uGDAZuNkPYUWXE-AiNh4T9_t6eWmf3thOFgzA"
+
 var searchFrom = document.getElementById('search_form');
 var searchBox = document.getElementById('search_box');
 var searchResult = document.getElementById('search_result');
 var searchShowBtn = document.getElementById('show_more_btn');
+
+let keyword = "";
+let page = 1;
+
+async function searchImage(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}`
+
+}

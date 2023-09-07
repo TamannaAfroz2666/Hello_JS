6561a3a29db76f419e15a8d5fbcd31f5cbba22ad
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-07 10:32:52 +0600

    access to all id field create

---
+++ ImageProcessing/base.js
@@ -0,0 +1,4 @@
+var searchFrom = document.getElementById('search_form');
+var searchBox = document.getElementById('search_box');
+var searchResult = document.getElementById('search_result');
+var searchShowBtn = document.getElementById('show_more_btn');

37fdb9c63b2807484dddede747922e86488966b5
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-07 18:24:27 +0600

    autoComplete using in input field

--- ImageProcessing/base.js
+++ ImageProcessing/base.js
@@ -1,15 +1,18 @@
-const accesKey ="weUuR1uGDAZuNkPYUWXE-AiNh4T9_t6eWmf3thOFgzA";
+const accesKey = "weUuR1uGDAZuNkPYUWXE-AiNh4T9_t6eWmf3thOFgzA";
 
 var searchFrom = document.getElementById('search_form');
 var searchBox = document.getElementById('search_box');
 console.log('search box', searchBox);
 var searchResult = document.getElementById('search_result');
 var searchShowBtn = document.getElementById('show_more_btn');
+// var searchBtn = document.getElementById('search_btn');
 
 let keyword = "";
 let page = 1;
 
-async function searchImage(){
+async function searchImage() {
+    console.log('search image called')
+
     keyword = searchBox.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesKey}&per_page=12`;
 
@@ -20,31 +23,28 @@ async function searchImage(){
     console.log(data);
     const dataResult = data.results;
     console.log('result is', dataResult);
-    dataResult.map((result) =>{
-         const image = document.createElement('img');
-         image.src = result.urls.small;
-         const imageLink = document.createElement('a');
-         imageLink.href = result.links.html;
-         imageLink.target = '_blank';
-         //append child from img to ankor tag
-         imageLink.appendChild(image);
+    dataResult.map((result) => {
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = '_blank';
+        //append child from img to ankor tag
+        imageLink.appendChild(image);
         //append child from img to show tag
-         searchResult.appendChild(imageLink);
+        searchResult.appendChild(imageLink);
 
     })
-    searchShowBtn.style.display = 'block';        
+    searchShowBtn.style.display = 'block';
 }
-
-if(searchFrom){
-searchFrom.addEventListener("submit", (e) =>{
+searchFrom.addEventListener("submit", (e) => {
     e.preventDefault();
-    page = 1;
-    searchImage();
-})
-}
+    searchImage()
+    // handle submit
+});
 
-if(searchShowBtn){
-    searchShowBtn.addEventListener("click", () =>{
+if (searchShowBtn) {
+    searchShowBtn.addEventListener("click", () => {
         page++;
         searchImage();
 

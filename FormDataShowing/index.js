03126f6dd613fedac332266b4f9fa9d73e45680a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-09 13:17:46 +0600

    button onclick event work

--- FormDataShowing/index.js
+++ FormDataShowing/index.js
@@ -1,18 +1,57 @@
 var formMain = document.getElementById('form_main');
-// console.log(t);
-var genderList = document.getElementById('gender_list');
-// console.log('gender is', genderList);
-var firstName = document.getElementById('first_name');
-var initialName = document.getElementById('initials_name');
-var lastName = document.getElementById('last_name');
-var customerAddress = document.getElementById('customer_add');
-var customerAddress2 = document.getElementById('customer_add2');
-var City = document.getElementById('city_id');
-var Region = document.getElementById('region_id');
-var postalCode = document.getElementById('postal_id');
-var countryPicker = document.getElementById('countryPicker');
-var customerNumber = document.getElementById('number_id');
-var customerFax = document.getElementById('fax_id');
-var calenderDate = document.getElementById('calender_id');
-var emailId = document.getElementById('email_id');
-var checkBox = document.getElementById('horns');
+
+
+
+
+
+
+
+function send_info() {
+    
+    console.log('hello js');
+    let firstName = document.getElementById('first_name').value;
+    console.log('first name is:', firstName);
+    // e.preventDefault()
+}
+
+
+// function send_info() {
+//     var genderList = document.getElementById('gender_list').value;
+//     var initialName = document.getElementById('initials_name').value;
+//     var firstName = document.getElementById('first_name').value;
+//     var lastName = document.getElementById('last_name').value;
+//     var customerAddress = document.getElementById('customer_add').value;
+//     var customerAddress2 = document.getElementById('customer_add2').value;
+//     var City = document.getElementById('city_id').value;
+//     var Region = document.getElementById('region_id').value;
+//     var postalCode = document.getElementById('postal_id').value;
+//     var countryPicker = document.getElementById('countryPicker').value;
+//     var customerNumber = document.getElementById('number_id').value;
+//     var customerFax = document.getElementById('fax_id').value;
+//     var calenderDate = document.getElementById('calender_id').value;
+//     var emailId = document.getElementById('email_id').value;
+//     var checkBox = document.getElementById('horns').value;
+//     console.log('the gender is:', genderList);
+//     console.log('the initials name is:', initialName);
+//     console.log('the first name is:', firstName);
+//     console.log('the last name is:', lastName);
+//     console.log('Customer address is:', customerAddress);
+//     console.log('Customer address is:', customerAddress2);
+//     console.log('City is:', City);
+//     console.log('the region is:', Region);
+//     console.log('the postal code is:', postalCode);
+//     console.log('thecountry piker is:', countryPicker);
+//     console.log('the number  is:', customerNumber);
+//     console.log('the fax is:', customerFax);
+//     console.log('the calender date is:', calenderDate);
+//     console.log('the email id is:', emailId);
+//     console.log('the checkbox is:', checkBox);
+   
+
+// }
+
+// formMain.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     send_info();
+// })
+

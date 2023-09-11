d0a926b4401286d801f544f78877a7bb62e85873
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-11 15:39:40 +0600

    deposit amount coverted to string to int

--- MoneyCalculations/baseMoney.js
+++ MoneyCalculations/baseMoney.js
@@ -1,8 +1,19 @@
+
+
 function depositAmount(){
+    var addvalue = 0;
+
     let depositAmount = document.getElementById('depositeAmmount').value;
     console.log('deposite amount is', depositAmount);
-    let showDeposite = document.getElementById('show_deposite').innerHTML = depositAmount;
+    // Add string to integer value 
+    const newDepositAmount = parseFloat(depositAmount);  
+    console.log('Amount will be string to int', newDepositAmount);
+
+    let showDeposite = document.getElementById('show_deposite').innerHTML = newDepositAmount;
     console.log('Show deposite amount is', showDeposite);
-    let showBalance = document.getElementById('show_balance').innerText = depositAmount;
+    let showBalance = document.getElementById('show_balance').innerText = newDepositAmount;
     console.log('Show balance is', showBalance);
+
+    // let addvalue = depositAmount + addvalue;
+    // console.log('total value is', addvalue);
 }

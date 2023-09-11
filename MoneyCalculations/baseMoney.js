5e892a74fc829ad21106435482dccb1b51c786f0
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-11 16:16:41 +0600

    deposit part calculate

--- MoneyCalculations/baseMoney.js
+++ MoneyCalculations/baseMoney.js
@@ -1,22 +1,30 @@
 
 
 function depositAmount(){
-    var addvalue = 0;
 
     let depositAmount = document.getElementById('depositeAmmount').value;
     console.log('deposite amount is', depositAmount);
     // Add string to integer value 
     const newDepositAmount = parseFloat(depositAmount);  
     console.log('Amount will be string to int', newDepositAmount);
-
-    let showDeposite = document.getElementById('show_deposite').innerHTML = newDepositAmount;
-    console.log('Show deposite amount is', showDeposite);
+    //show deposit
+    let depositTotalElement = document.getElementById('show_deposite');
+    const priviousDepositTotalString =depositTotalElement.innerText;
+    const priviousDepositTotal =parseFloat(priviousDepositTotalString);
+    console.log('priviois amount is', priviousDepositTotal);
+    // let showDeposite = document.getElementById('show_deposite').innerHTML = newDepositAmount;
+    // console.log('Show deposite amount is', showDeposite);
   
-  
+  //show balance
     let showBalance = document.getElementById('show_balance').innerText = newDepositAmount;
     console.log('Show balance is', showBalance);
 
     // set the empaty string when click the button to get the vlue 
     depositeAmmount.value = '';
+
+    //total deposit calculate
+
+    const newDepositTotal = priviousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = newDepositTotal
    
 }

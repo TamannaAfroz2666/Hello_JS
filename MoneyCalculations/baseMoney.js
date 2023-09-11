8e46629fdffcd83bcf4d008123c0edabca31d243
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-11 16:45:14 +0600

    code cleanning

--- MoneyCalculations/baseMoney.js
+++ MoneyCalculations/baseMoney.js
@@ -12,19 +12,15 @@ function depositAmount(){
     const priviousDepositTotalString =depositTotalElement.innerText;
     const priviousDepositTotal =parseFloat(priviousDepositTotalString);
     console.log('priviois amount is', priviousDepositTotal);
-    // let showDeposite = document.getElementById('show_deposite').innerHTML = newDepositAmount;
-    // console.log('Show deposite amount is', showDeposite);
-  
-  //show balance
-    let showBalance = document.getElementById('show_balance').innerText = newDepositAmount;
-    console.log('Show balance is', showBalance);
-
+    
     // set the empaty string when click the button to get the vlue 
     depositeAmmount.value = '';
 
     //total deposit calculate
 
     const newDepositTotal = priviousDepositTotal + newDepositAmount;
-    depositTotalElement.innerText = newDepositTotal
+    depositTotalElement.innerText = newDepositTotal;
+
+      //show balance
    
 }

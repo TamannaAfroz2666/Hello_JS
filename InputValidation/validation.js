ce145a45203e6a20c8d974d789293e0eb742e6ff
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-12 13:32:43 +0600

    check the validate email on main function

--- InputValidation/validation.js
+++ InputValidation/validation.js
@@ -1,14 +1,20 @@
 function submit_info (){
     // e.preventDefault()
     console.log('clicked');
-    let username = document.getElementById('user_id').value;
+    let username = document.getElementById('user_email').value;
     console.log('user name is :', username);
     const password = document.getElementById('password_id').value;
     console.log('password is', password);
 
     // value refresh when get a needed one
-    user_id.value = '';
+    user_email.value = '';
     password_id.value = '';
+
+    // check email valid or not 
+    if(!validateEmail(user_email.value)){
+        alert('please enter a valid email address')
+        return;
+    }
     
 }
 

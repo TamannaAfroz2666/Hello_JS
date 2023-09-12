d09cb153986b080123ab3ec867136cda00784f3a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-12 15:22:03 +0600

    fixed issue using addeventlistner

--- InputValidation/validation.js
+++ InputValidation/validation.js
@@ -1,44 +1,50 @@
-function submit_info (){
-    // e.preventDefault()
-    console.log('clicked');
-    let username = document.getElementById('user_email').value;
-    console.log('user name is :', username);
-    const password = document.getElementById('password_id').value;
-    console.log('password is', password);
+// function submit_info (){
+//     // e.preventDefault()
+//     console.log('clicked');
+//     let useremail = document.getElementById('email').value;
+//     console.log('user email is :', useremail);
+//     const passwordId = document.getElementById('password').value;
+//     console.log('password is', passwordId);
 
-    const errorMessages = document.getElementById('errorMessages');
+//     const errorMessages = document.getElementById('errorMessages');
 
-    // value refresh when get a needed one
-    user_email.value = '';
-    password_id.value = '';
+//     // value refresh when get a needed one
+//     email.value = '';
+//     password.value = '';
 
-    // check email valid or not 
-    if(!validateEmail(user_email.value)){
-        // alert('please enter a valid email address');
-        errorMessages.innerHTML += '<p>Please enter a valid email address.</p>';
-        return;
-    }
-    if(! validatePassword(password_id.value)){
-        // alert ('Please enter a valid password');
-        errorMessages.innerHTML += '<p>Please enter a valid password address.</p>';
-        return;
-    }
-    alert('login successfully');
-}
+//     // check email valid or not 
+//     if(!validateEmail(email.value)){
+//         alert('please enter a valid email address');
+//         // errorMessages.innerHTML += 'Please enter a valid email address.';
+//         // return;
+//     }
+//    if(!validatePassword(password.value)){
+//         alert ('Please enter a valid password');
+//         // errorMessages.innerHTML += 'Please enter a valid password address';
+//         // return;
+//     }
+//     else{
+//         alert('login successfully');
 
-//function valid emation create
+//     }
+   
+// }
 
-function validateEmail(email){
-    const emailRange = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRange.test(email);
-}
+// //function valid emation create
 
-function validatePassword(password){
-    const minLength = 8;
-    const upperCase =/[A-Z]/.test(password);
-    const lowerCase = /[a-z]/.test(password);
-    const hasNumber = /\d/.test(password);
+// function validateEmail(email){
+//     const emailRange = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+//     return emailRange.test(email);
+// }
 
-    return (password.length >= minLength && upperCase && lowerCase && hasNumber);
+// function validatePassword(password){
+//     const minLength = 8;
+//     const upperCase =/[A-Z]/.test(password);
+//     const lowerCase = /[a-z]/.test(password);
+//     const hasNumber = /\d/.test(password);
 
-}
+//     return (
+//         password.length >= minLength && upperCase && lowerCase && hasNumber
+//         );
+
+// }
---
+++ InputValidation/validationNew.js
@@ -0,0 +1,55 @@
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', validateLoginForm);
+
+function validateLoginForm(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const errorMessages = document.getElementById('errorMessages');
+    const errorMessages1 = document.getElementById('errorMessages1');
+    errorMessages.innerHTML = '';
+    errorMessages1.innerHTML = '';
+
+
+    // Validate email
+    if (!validateEmail(email.value) && !validatePassword(password.value)) {
+        errorMessages.innerHTML += 'Please enter a valid email address.';
+        errorMessages1.innerHTML += 'Please enter a valid password.';
+        return;
+    }
+    else if (!validateEmail(email.value)) {
+        errorMessages.innerHTML += 'Please enter a valid email address.';
+        return;
+    }
+
+    // Validate password
+    else if (!validatePassword(password.value)) {
+        errorMessages1.innerHTML += 'Please enter a valid password.';
+        return;
+    }
+
+    // If all validations pass, submit the form
+    alert('Login successful!');
+}
+
+function validateEmail(email) {
+    // Use a regular expression to check if the email is valid
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    // Customize these rules to match your password requirements
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+
+    return (
+        password.length >= minLength &&
+        hasUpperCase &&
+        hasLowerCase &&
+        hasNumber
+    );
+}

9fbce990c70439fb2b3d8855b021c9157ee6895b
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-12 13:11:20 +0600

    add refresh input field

--- InputValidation/validation.js
+++ InputValidation/validation.js
@@ -4,4 +4,9 @@ function submit_info (){
     console.log('user name is :', username);
     const password = document.getElementById('password_id').value;
     console.log('password is', password);
+
+    // value refresh when get a needed one
+    user_id.value = '';
+    password_id.value = '';
+    
 }

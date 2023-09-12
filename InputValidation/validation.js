753e595b31db736c97d426dbc55bd3de80e66c95
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-12 13:06:03 +0600

    access the input password field

---
+++ InputValidation/validation.js
@@ -0,0 +1,7 @@
+function submit_info (){
+    console.log('clicked');
+    let username = document.getElementById('user_id').value;
+    console.log('user name is :', username);
+    const password = document.getElementById('password_id').value;
+    console.log('password is', password);
+}

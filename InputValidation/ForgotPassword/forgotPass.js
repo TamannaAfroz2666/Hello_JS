772a9b3948d21665c2b296d55b5b5a8a621de685
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-13 17:00:01 +0600

    issue fixed

--- InputValidation/ForgotPassword/forgotPass.js
+++ InputValidation/ForgotPassword/forgotPass.js
@@ -3,9 +3,20 @@
 function submitHere(){
     console.log('here');
     const newPasswordd = document.getElementById('password').value;
+    console.log('new password', newPasswordd);
     const ReNewPassword = document.getElementById('reTypepass').value;
+    console.log('Renew Password', ReNewPassword);
 
     //get error message
     const errorMessages = document.getElementById('errorMessages');
     errorMessages.innerHTML = '';
+    
+    if(!(newPasswordd === ReNewPassword)){
+        errorMessages.innerHTML += 'Not Match password'
+    }
+    else{
+        alert('successfully done')
+
+    }
+    
 }

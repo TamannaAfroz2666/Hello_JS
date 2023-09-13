34e6564e98efd64703fcd87018d33d845806ea82
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-13 16:50:18 +0600

    acces all field

---
+++ InputValidation/ForgotPassword/forgotPass.js
@@ -0,0 +1,11 @@
+
+
+function submitHere(){
+    console.log('here');
+    const newPasswordd = document.getElementById('password').value;
+    const ReNewPassword = document.getElementById('reTypepass').value;
+
+    //get error message
+    const errorMessages = document.getElementById('errorMessages');
+    errorMessages.innerHTML = '';
+}

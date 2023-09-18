debaf31f8d97bd95a7eedaa268d3df6a8dc3ecfc
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-18 17:33:41 +0600

    headphone price value access

--- ShoppingCard/JS/card.js
+++ ShoppingCard/JS/card.js
@@ -1,44 +1,50 @@
 
+
 // ----start -----
 function getInputValue() {
     const priviousFunc = inputValueOnChange();
-    //price value get
+    //price value get for walton
     const priceValueStr = document.getElementById('priceWL').innerText;
     const priceValue = parseFloat(priceValueStr);
-    console.log('walton product price is:', priceValue);
-    // return priceValue;
 
+     //price value get for headphone
+    const priceHeadPhoStr = document.getElementById('priceHd').innerText;
+    const priceHeadPho = parseFloat(priceHeadPhoStr);
+    console.log('the headphone price is :', priceHeadPho);
+
+
+    // console.log('walton product price is:', priceValue);
+    // calculate total price 
     const totalPrice = priviousFunc * priceValue;
-    console.log('total price is:', totalPrice);
+    // console.log('total price is:', totalPrice);
 
 
 }
 
 
-
-
 //  Walton input value get 
 function inputValueOnChange() {
     const productNumber = document.getElementById('waltonValue');
-
     const productNumberStr = productNumber.value;
-    const productNumberValue = parseInt(productNumberStr)
-    console.log('Input product number is:', productNumberValue);
+    const productNumberValue = parseInt(productNumberStr);
+    // console.log('Input product number is:', productNumberValue);
 
     // Quantity passing 
 
     const cardShow = document.getElementById('cartShowQuantity').innerText = productNumberValue;
-    console.log('innerText is :', cardShow);
+    // console.log('innerText is :', cardShow);
 
     //access price field
 
     const priceValueStr = document.getElementById('priceWL').innerText;
     const priceValue = parseFloat(priceValueStr);
-    console.log('walton product price is:', priceValue);
+    // console.log('walton product price is:', priceValue);
+
+    // calculate total price 
 
     const totalPrice = productNumberValue * priceValue;
-    console.log('total price is:', totalPrice);
-    
+    // console.log('total price is:', totalPrice);
+
     return productNumberValue;
 
 
@@ -50,4 +56,11 @@ function addAcardWalton() {
     console.log('hee');
     getInputValue();
     inputValueOnChange();
+}
+
+
+// for  headphone function 
+function addCartHeadPho(){
+    // console.log('headphone is here');
+    getInputValue();
 }

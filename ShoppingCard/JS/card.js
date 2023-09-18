9342c25b1cda30760142c20ad069e673adbcd9a0
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-18 16:29:34 +0600

    fixed the onchange state calculations

--- ShoppingCard/JS/card.js
+++ ShoppingCard/JS/card.js
@@ -1,39 +1,53 @@
 
 // ----start -----
-function getInputValue(){
-  const priviousFunc = inputValue();
+function getInputValue() {
+    const priviousFunc = inputValueOnChange();
     //price value get
     const priceValueStr = document.getElementById('priceWL').innerText;
     const priceValue = parseFloat(priceValueStr);
     console.log('walton product price is:', priceValue);
+    // return priceValue;
 
-    //calculate the price with product quantity
-    const totalPrice = priviousFunc * priceValue; 
+    const totalPrice = priviousFunc * priceValue;
     console.log('total price is:', totalPrice);
 
-  
+
 }
 
+
+
+
 //  Walton input value get 
-function inputValue(){
+function inputValueOnChange() {
     const productNumber = document.getElementById('waltonValue');
-    const  productNumberStr = productNumber.value;
+
+    const productNumberStr = productNumber.value;
     const productNumberValue = parseInt(productNumberStr)
-    console.log('Input product number is:', productNumberValue); 
+    console.log('Input product number is:', productNumberValue);
 
     // Quantity passing 
 
     const cardShow = document.getElementById('cartShowQuantity').innerText = productNumberValue;
     console.log('innerText is :', cardShow);
+
+    //access price field
+
+    const priceValueStr = document.getElementById('priceWL').innerText;
+    const priceValue = parseFloat(priceValueStr);
+    console.log('walton product price is:', priceValue);
+
+    const totalPrice = productNumberValue * priceValue;
+    console.log('total price is:', totalPrice);
+    
     return productNumberValue;
 
-      
+
 
 }
 
 // Main function or button 
-function addAcardWalton(){
+function addAcardWalton() {
     console.log('hee');
     getInputValue();
-    inputValue();
+    inputValueOnChange();
 }

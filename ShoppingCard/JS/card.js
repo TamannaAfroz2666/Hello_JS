2b9773ff657456fc1f622965ddb2ed9f362e9c62
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-18 12:37:02 +0600

    add card id access

--- ShoppingCard/JS/card.js
+++ ShoppingCard/JS/card.js
@@ -8,3 +8,14 @@
 //     console.log('Buy now here');
 
 // }
+// ----start -----
+function getInputValue(){
+    const productNumber = document.getElementById('waltonValue');
+    const  productNumberValue = productNumber.value;
+    console.log('Input product number is:', productNumberValue);
+}
+
+function addAcardWalton(){
+    console.log('hee');
+    getInputValue();
+}

cc0eda2ad2c888e92d2bd3a802828d0c8a68f94f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-18 12:46:52 +0600

    convert into str to int

--- ShoppingCard/JS/card.js
+++ ShoppingCard/JS/card.js
@@ -2,14 +2,17 @@
 // ----start -----
 function getInputValue(){
     const productNumber = document.getElementById('waltonValue');
-    const  productNumberValue = productNumber.value;
+    const  productNumberStr = productNumber.value;
+    const productNumberValue = parseInt(productNumberStr)
     console.log('Input product number is:', productNumberValue);
 
     //price value get
-    const priceValue = document.getElementById('priceWL').innerText;
+    const priceValueStr = document.getElementById('priceWL').innerText;
+    const priceValue = parseFloat(priceValueStr);
     console.log('walton product price is:', priceValue);
 
     //calculate the price with product quantity
+    // const totalPrice = 
 
 
 }

2f03fd40e27387acad73fa7a31d461331307e39c
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-19 16:01:20 +0600

    new fie

--- ShoppingCard/JS/card.js
+++ ShoppingCard/JS/card.js
@@ -7,14 +7,15 @@ function getInputValue() {
     const priceValueStr = document.getElementById('priceWL').innerText;
     const priceValue = parseFloat(priceValueStr);
 
-     //price value get for headphone
+    //price value get for headphone
     const priceHeadPhoStr = document.getElementById('priceHd').innerText;
     const priceHeadPho = parseFloat(priceHeadPhoStr);
     console.log('the headphone price is :', priceHeadPho);
 
     // calculate total price 
     const totalPrice = priviousFunc * priceValue;
-    
+
+
 
 
 
@@ -23,29 +24,33 @@ function getInputValue() {
 
 //  Walton input value onChange field 
 function inputValueOnChange() {
-    var cardShowInnerTest = 0;
+
     const productNumber = document.getElementById('waltonValue');
     const productNumberStr = productNumber.value;
     const productNumberValue = parseInt(productNumberStr);
-    // console.log('Input product number is:', productNumberValue);
+
 
     // Quantity passing 
 
     // const cardShow = document.getElementById('cartShowQuantity').innerText = productNumberValue;
-    const cardShow = document.getElementById('cartShowQuantity');
-    const cardShowInnerStr = cardShow.innerText;
-    const cardShowInner = parseInt(cardShowInnerStr);
-    console.log('innerText is :', cardShow);
+    // const cardShow = document.getElementById('cartShowQuantity');
+    // const cardShowInnerStr = cardShow.innerText;
+    // const cardShowInner = parseInt(cardShowInnerStr);
+
 
 
     // calculate the input value and card value 
-    const newSumValue = productNumberValue + cardShowInnerTest;
-    cardShow.innerText = newSumValue;
+    const newSumValue = productNumberValue;
+    console.log("new sum value = ", newSumValue)
+
+
+    cardShowInnerTest1 = newSumValue;
+    document.getElementById('cartShowQuantity').innerText = cardShowInnerTest1;
 
     //access price field
     const priceValueStr = document.getElementById('priceWL').innerText;
     const priceValue = parseFloat(priceValueStr);
-    // console.log('walton product price is:', priceValue);
+
 
     // calculate total price 
 
@@ -59,13 +64,26 @@ function inputValueOnChange() {
 }
 
 //headphone onchange field
-function inputChangeField(){
+function inputChangeField() {
+
     const headphoneValueStr = document.getElementById('headPhoValue').value;
     const headphoneValue = parseInt(headphoneValueStr);
     console.log('headphone input value is', headphoneValue);
 
     // QUANTITY PASSS
-    const quantityPass = document.getElementById('cartShowQuantity').innerText = headphoneValue;
+    // const quantityPass = document.getElementById('cartShowQuantity').innerText = headphoneValue;
+
+
+
+
+
+    // calculate the input value and card value 
+    const newSumValue1 = headphoneValue ;
+    // cardShow.innerText = newSumValue1;
+
+    cardShowInnerTest1 = cardShowInnerTest1
+    document.getElementById('cartShowQuantity').innerText = cardShowInnerTest1;
+
     // return quantityPass;
 }
 
@@ -74,13 +92,14 @@ function inputChangeField(){
 function addAcardWalton() {
     // console.log('hee');
     getInputValue();
-    inputValueOnChange();
+
 }
 
 
 // for  headphone function 
-function addCartHeadPho(){
+function addCartHeadPho() {
     // console.log('headphone is here');
     getInputValue();
-    inputChangeField();
+    // inputChangeField();
 }
+

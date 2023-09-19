0039b014b61c06b25507cf55669ff7926704d902
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-19 16:06:40 +0600

    add new function create

---
+++ ShoppingCard/JS/ShoppingCard.js
@@ -0,0 +1,21 @@
+
+
+// -----start ------//
+
+function cardValueChanged(){
+    console.log('click');
+}
+
+// Main function or button 
+function addCardWalton() {
+ 
+    cardValueChanged();
+
+}
+
+
+// for  headphone function 
+function addCartHeadPho() {
+    
+    cardValueChanged();
+}

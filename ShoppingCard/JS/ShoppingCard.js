0abd94b3807d1f064e12a90363c111ab6310ba54
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-19 19:05:04 +0600

    access to walton field

--- ShoppingCard/JS/ShoppingCard.js
+++ ShoppingCard/JS/ShoppingCard.js
@@ -4,11 +4,36 @@
 
 function cardValueChanged(){
     console.log('click');
+    // -------for walton part----
+
+    //price value get for walton
+    const priceValueStr = document.getElementById('priceWL').innerText;
+    const priceValue = parseFloat(priceValueStr);
+    // Walton input onchange value 
+    const productNumber = document.getElementById('waltonValue');
+    const productNumberStr = productNumber.value;
+    const productNumberValue = parseInt(productNumberStr);
+
+       // -------for walton part----
+
+
+
+
+
+
+
+
+
+    //price value get for headphone
+    const priceHeadPhoStr = document.getElementById('priceHd').innerText;
+    const priceHeadPho = parseFloat(priceHeadPhoStr);
+    console.log('the headphone price is :', priceHeadPho);
+
+
 }
 
 // Main function or button 
 function addCardWalton() {
- 
     cardValueChanged();
 
 }
@@ -16,6 +41,5 @@ function addCardWalton() {
 
 // for  headphone function 
 function addCartHeadPho() {
-    
     cardValueChanged();
 }
